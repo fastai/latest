191196dffc8638b273c4893ae202179a1f648cee
Jeremy Howard <j@fast.ai>
2022-04-27 17:26:40 -0400

    search

--- latest.py
+++ latest.py
@@ -5,8 +5,11 @@ from fastcore.all import *
 @fastcgi('/var/run/caddy/fcgi.sock')
 def run():
     print(f'Content-type: text/plain\n')
-    try: _,user,repo = os.environ['PATH_INFO'].split('/')
+    try: _,user,repo,*search = os.environ['PATH_INFO'].split('/')
     except ValueError: return print('!!!Invalid path')
-    try: rels=urljson(f'https://api.github.com/repos/{user}/{repo}/releases/latest')['assets']
+    gh = 'https://api.github.com/repos'
+    try: rels=urljson(f'{gh}/{user}/{repo}/releases/latest')['assets']
     except: return print(f'!!!Could not access {user}/{repo}')
-    print('\n'.join(L(rels).itemgot('browser_download_url')))
+    urls = L(rels).itemgot('browser_download_url')
+    if search: urls = [o for o in urls if search[0] in o]
+    print('\n'.join(urls))
