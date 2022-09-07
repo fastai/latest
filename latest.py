6575b9bcb9ccfe30e0dd8a1721cc2ee866b20b1d
Jeremy Howard <j@fast.ai>
2022-09-06 21:22:28 -0400

    pre

--- latest.py
+++ latest.py
@@ -5,11 +5,14 @@ from fastcore.all import *
 @fastcgi('/var/run/caddy/fcgi.sock')
 def run():
     print(f'Content-type: text/plain\n')
-    try: _,user,repo,*search = os.environ['PATH_INFO'].split('/')
+    try: _,pre,user,repo,*search = os.environ['PATH_INFO'].split('/')
     except ValueError: return print('!!!Invalid path')
-    gh = 'https://api.github.com/repos'
-    try: rels=urljson(f'{gh}/{user}/{repo}/releases/latest')['assets']
-    except: return print(f'!!!Could not access {user}/{repo}')
-    urls = L(rels).itemgot('browser_download_url')
+    if pre not in ('pre', 'latest'): return print('!!!first component should be "pre" or "latest"')
+    gh = f'https://api.github.com/repos/{user}/{repo}/releases'
+    if pre=='latest': gh += '/latest'
+    try: rels=urljson(gh)
+    except Exception as e: return print(f'!!!Could not access {gh}; {e}')
+    if pre=='pre': rels = rels[0]
+    urls = L(rels['assets']).itemgot('browser_download_url')
     if search: urls = [o for o in urls if search[0] in o]
     print('\n'.join(urls))
