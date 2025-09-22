ddab92f783f01f5e77528886a7066d86c5ed7b3f
Jeremy Howard <github@jhoward.fastmail.fm>
2025-09-23 09:59:08 +1000

    fasthtml

--- latest.py
+++ latest.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python
-from fastcgi import *
+import re
+from fasthtml.common import *
 from fastcore.all import *
 
-@fastcgi('/var/run/caddy/fcgi.sock')
-def run():
-    print(f'Content-type: text/plain\n')
-    try: _,pre,user,repo,*search = os.environ['PATH_INFO'].split('/')
-    except ValueError: return print('!!!Invalid path')
-    if pre not in ('pre', 'latest'): return print('!!!first component should be "pre" or "latest"')
+app = FastHTML()
+rt = app.route
+
+@rt('/{path:path}')
+def index(path:str):
+    try: pre,user,repo,*search = path.split('/')
+    except ValueError: return '!!!Invalid path'
+    if pre not in ('pre', 'latest'): return '!!!first component should be "pre" or "latest"'
     gh = f'https://api.github.com/repos/{user}/{repo}/releases'
     if pre=='latest': gh += '/latest'
     try: rels=urljson(gh)
-    except Exception as e: return print(f'!!!Could not access {gh}; {e}')
+    except Exception as e: return f'!!!Could not access {gh}; {e}'
     if pre=='pre': rels = rels[0]
     urls = L(rels['assets']).itemgot('browser_download_url')
-    if search: urls = [o for o in urls if search[0] in o]
-    print('\n'.join(urls))
+    if search: urls = [o for o in urls if re.search(search[0], o)]
+    return '\n'.join(urls)
+
+serve()
+
